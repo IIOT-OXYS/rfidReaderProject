18844d12e7ab6f43ab843d0fcee4f0b6450a04c7
arades79 <arades79@gmail.com>
2017-08-11 11:26:45 -0400

    re-add static IP assignment class, create empty AsyncTasks for all known API methods

--- ToyotaRFID/app/src/main/java/com/example/nzar/toyotarfid/DatabaseConnector.java
+++ ToyotaRFID/app/src/main/java/com/example/nzar/toyotarfid/DatabaseConnector.java
@@ -3,8 +3,21 @@
 //2017
 package com.example.nzar.toyotarfid;
 
+import android.content.Context;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.example.nzar.toyotarfid.SettingsActivity.settings;
 
 /**
  * Created by cravers on 6/29/2017.
@@ -82,17 +95,127 @@ class DatabaseConnector extends AppCompatActivity {
 //
 //    }
 
-    /*
-        This method will query the database specified to see if the user that just badged in is
-        authorized to use the device. It will also set up the currentEmployee to the row obtained.
-     */
 
-    public class TILTAPIPostUser extends AsyncTask<Integer, String, Boolean> {
+//give the badge number as a string, provide progress messages as Strings, and return a Boolean if the user is allowed
+    public class TILTPostUserTask extends AsyncTask<String, String, Boolean> {
         @Override
-        protected Boolean doInBackground(Integer... params) {
+        protected Boolean doInBackground(String... params) {
 
             return false;
         }
+    }
+
+    public class TILTGetUserTask extends AsyncTask<Void,Void,Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            return null;
+        }
+    }
+
+    public class TILTGetTechTask extends AsyncTask<Void,Void,Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            return null;
+        }
+    }
+
+    public class SetNetworkConfig extends AsyncTask<Void,Void,Boolean> {
+        @Override
+        protected Boolean doInBackground(Void... params) {
+
+            WifiConfiguration wifiConf = null;
+            WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+            WifiInfo connectionInfo = wifiManager.getConnectionInfo();
+            List<WifiConfiguration> configuredNetworks = wifiManager.getConfiguredNetworks();
+            for (WifiConfiguration conf : configuredNetworks) {
+                if (conf.networkId == connectionInfo.getNetworkId()) {
+                    wifiConf = conf;
+                    break;
+                }
+            }
+            try {
+                NetworkConfigurator.setIpAssignment("STATIC", wifiConf); //or "DHCP" for dynamic setting
+                NetworkConfigurator.setIpAddress(InetAddress.getByName(settings.getString("static_ip", "192.168.0.235")), 24, wifiConf);
+                NetworkConfigurator.setDNS(InetAddress.getByName("8.8.8.8"), wifiConf);
+                wifiManager.updateNetwork(wifiConf); //apply the setting
+                wifiManager.saveConfiguration(); //Save it
+            } catch (Exception e) {
+                e.printStackTrace();
+                return false;
+            }
+
+            return true;
+        }
+    }
+
+
+
+    @SuppressWarnings("unchecked")
+    static class NetworkConfigurator {
+
+        static void setIpAssignment(String assign, WifiConfiguration wifiConf)
+                throws SecurityException, IllegalArgumentException, NoSuchFieldException, IllegalAccessException {
+            setEnumField(wifiConf, assign, "ipAssignment");
+        }
+
+        static void setIpAddress(InetAddress addr, int prefixLength, WifiConfiguration wifiConf)
+                throws SecurityException, IllegalArgumentException, NoSuchFieldException, IllegalAccessException,
+                NoSuchMethodException, ClassNotFoundException, InstantiationException, InvocationTargetException {
+            Object linkProperties = getField(wifiConf, "linkProperties");
+            if (linkProperties == null) return;
+            Class laClass = Class.forName("android.net.LinkAddress");
+            Constructor laConstructor = laClass.getConstructor(InetAddress.class, int.class);
+            Object linkAddress = laConstructor.newInstance(addr, prefixLength);
+
+            ArrayList mLinkAddresses = (ArrayList) getDeclaredField(linkProperties, "mLinkAddresses");
+            mLinkAddresses.clear();
+            mLinkAddresses.add(linkAddress);
+        }
+
+        public static void setGateway(InetAddress gateway, WifiConfiguration wifiConf)
+                throws SecurityException, IllegalArgumentException, NoSuchFieldException, IllegalAccessException,
+                ClassNotFoundException, NoSuchMethodException, InstantiationException, InvocationTargetException {
+            Object linkProperties = getField(wifiConf, "linkProperties");
+            if (linkProperties == null) return;
+            Class routeInfoClass = Class.forName("android.net.RouteInfo");
+            Constructor routeInfoConstructor = routeInfoClass.getConstructor(InetAddress.class);
+            Object routeInfo = routeInfoConstructor.newInstance(gateway);
+
+            ArrayList mRoutes = (ArrayList) getDeclaredField(linkProperties, "mRoutes");
+            mRoutes.clear();
+            mRoutes.add(routeInfo);
+        }
+
+        static void setDNS(InetAddress dns, WifiConfiguration wifiConf)
+                throws SecurityException, IllegalArgumentException, NoSuchFieldException, IllegalAccessException {
+            Object linkProperties = getField(wifiConf, "linkProperties");
+            if (linkProperties == null) return;
+
+            ArrayList<InetAddress> mDnses = (ArrayList<InetAddress>) getDeclaredField(linkProperties, "mDnses");
+            mDnses.clear(); //or add a new dns address , here I just want to replace DNS1
+            mDnses.add(dns);
+        }
+
+        static Object getField(Object obj, String name)
+                throws SecurityException, NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
+            Field f = obj.getClass().getField(name);
+            return f.get(obj);
+        }
+
+        static Object getDeclaredField(Object obj, String name)
+                throws SecurityException, NoSuchFieldException,
+                IllegalArgumentException, IllegalAccessException {
+            Field f = obj.getClass().getDeclaredField(name);
+            f.setAccessible(true);
+            return f.get(obj);
+        }
+
+        static void setEnumField(Object obj, String value, String name)
+                throws SecurityException, NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
+            Field f = obj.getClass().getField(name);
+            f.set(obj, Enum.valueOf((Class<Enum>) f.getType(), value));
+        }
+
     }
 
 
