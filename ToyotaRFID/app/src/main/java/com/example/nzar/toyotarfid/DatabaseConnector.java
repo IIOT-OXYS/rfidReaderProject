9a982379e9baf21a83e842cfb51e74980e231bf7
arades79 <arades79@gmail.com>
2017-09-07 11:43:53 -0400

    add more cases to JSON parsing loops

--- ToyotaRFID/app/src/main/java/com/example/nzar/toyotarfid/DatabaseConnector.java
+++ ToyotaRFID/app/src/main/java/com/example/nzar/toyotarfid/DatabaseConnector.java
@@ -10,6 +10,7 @@ import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.AsyncTask;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Base64;
 import android.util.JsonReader;
@@ -63,7 +64,8 @@ class DatabaseConnector extends AppCompatActivity {
         boolean Restricted;
     }
 
-      
+
+    @Nullable
     private static JsonReader TILTAPITask(HttpURLConnection connection, String method) throws Exception {
         connection.setRequestMethod(method);
         connection.setRequestProperty("Authorization", "basic VElMVFdlYkFQSToxM1RJTFRXZWJBUEkxMw==");
@@ -134,6 +136,9 @@ class DatabaseConnector extends AppCompatActivity {
                         //if the response is not empty, set UserAuthorized to true
                         String key = Response.nextName();
                         switch (key) {
+                            case "PPEID":
+                                ppe.PPEID = Response.nextInt();
+                                break;
                             case "PPE":
                                 ppe.name = Response.nextString();
                                 break;
@@ -237,7 +242,7 @@ class DatabaseConnector extends AppCompatActivity {
                                 temp.email = ResponseReader.nextString();
                                 break;
                             case ("PhoneNumber"):
-                                temp.email = ResponseReader.nextString();
+                                temp.phoneNumber = ResponseReader.nextString();
                                 break;
                             case "Image":
                                 temp.Image = ImageParser(ResponseReader.nextString());
@@ -258,105 +263,6 @@ class DatabaseConnector extends AppCompatActivity {
 
             return null;
         }
-    }
-
-    //take the app context, doesn't provide updates, and return a boolean
-    public static class SetNetworkConfigTask extends AsyncTask<Context,Void,Boolean> {
-        @Override
-        protected Boolean doInBackground(Context... params) {
-
-            WifiConfiguration wifiConf = null;
-            WifiManager wifiManager = (WifiManager) params[0].getSystemService(Context.WIFI_SERVICE);
-            WifiInfo connectionInfo = wifiManager.getConnectionInfo();
-            List<WifiConfiguration> configuredNetworks = wifiManager.getConfiguredNetworks();
-            for (WifiConfiguration conf : configuredNetworks) {
-                if (conf.networkId == connectionInfo.getNetworkId()) {
-                    wifiConf = conf;
-                    break;
-                }
-            }
-            try {
-                NetworkConfigurator.setIpAssignment("STATIC", wifiConf); //or "DHCP" for dynamic setting
-                NetworkConfigurator.setIpAddress(InetAddress.getByName(settings.getString("static_ip", "192.168.0.235")), 24, wifiConf);
-                NetworkConfigurator.setDNS(InetAddress.getByName("8.8.8.8"), wifiConf);
-                wifiManager.updateNetwork(wifiConf); //apply the setting
-                wifiManager.saveConfiguration(); //Save it
-            } catch (Exception e) {
-                e.printStackTrace();
-                return false;
-            }
-
-            return true;
-        }
-    }
-
-
-    @SuppressWarnings("unchecked")
-    private static class NetworkConfigurator {
-
-        static void setIpAssignment(String assign, WifiConfiguration wifiConf)
-                throws SecurityException, IllegalArgumentException, NoSuchFieldException, IllegalAccessException {
-            setEnumField(wifiConf, assign, "ipAssignment");
-        }
-
-        static void setIpAddress(InetAddress addr, int prefixLength, WifiConfiguration wifiConf)
-                throws SecurityException, IllegalArgumentException, NoSuchFieldException, IllegalAccessException,
-                NoSuchMethodException, ClassNotFoundException, InstantiationException, InvocationTargetException {
-            Object linkProperties = getField(wifiConf, "linkProperties");
-            if (linkProperties == null) return;
-            Class laClass = Class.forName("android.net.LinkAddress");
-            Constructor laConstructor = laClass.getConstructor(InetAddress.class, int.class);
-            Object linkAddress = laConstructor.newInstance(addr, prefixLength);
-
-            ArrayList mLinkAddresses = (ArrayList) getDeclaredField(linkProperties, "mLinkAddresses");
-            mLinkAddresses.clear();
-            mLinkAddresses.add(linkAddress);
-        }
-
-        public static void setGateway(InetAddress gateway, WifiConfiguration wifiConf)
-                throws SecurityException, IllegalArgumentException, NoSuchFieldException, IllegalAccessException,
-                ClassNotFoundException, NoSuchMethodException, InstantiationException, InvocationTargetException {
-            Object linkProperties = getField(wifiConf, "linkProperties");
-            if (linkProperties == null) return;
-            Class routeInfoClass = Class.forName("android.net.RouteInfo");
-            Constructor routeInfoConstructor = routeInfoClass.getConstructor(InetAddress.class);
-            Object routeInfo = routeInfoConstructor.newInstance(gateway);
-
-            ArrayList mRoutes = (ArrayList) getDeclaredField(linkProperties, "mRoutes");
-            mRoutes.clear();
-            mRoutes.add(routeInfo);
-        }
-
-        static void setDNS(InetAddress dns, WifiConfiguration wifiConf)
-                throws SecurityException, IllegalArgumentException, NoSuchFieldException, IllegalAccessException {
-            Object linkProperties = getField(wifiConf, "linkProperties");
-            if (linkProperties == null) return;
-
-            ArrayList<InetAddress> mDnses = (ArrayList<InetAddress>) getDeclaredField(linkProperties, "mDnses");
-            mDnses.clear(); //or add a new dns address , here I just want to replace DNS1
-            mDnses.add(dns);
-        }
-
-        static Object getField(Object obj, String name)
-                throws SecurityException, NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
-            Field f = obj.getClass().getField(name);
-            return f.get(obj);
-        }
-
-        static Object getDeclaredField(Object obj, String name)
-                throws SecurityException, NoSuchFieldException,
-                IllegalArgumentException, IllegalAccessException {
-            Field f = obj.getClass().getDeclaredField(name);
-            f.setAccessible(true);
-            return f.get(obj);
-        }
-
-        static void setEnumField(Object obj, String value, String name)
-                throws SecurityException, NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
-            Field f = obj.getClass().getField(name);
-            f.set(obj, Enum.valueOf((Class<Enum>) f.getType(), value));
-        }
-
     }
 
 
