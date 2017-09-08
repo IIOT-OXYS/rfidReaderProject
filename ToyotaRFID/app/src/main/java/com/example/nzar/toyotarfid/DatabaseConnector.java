e9923398378c4a6e8a819b4498ab25aa3f05a31f
arades79 <arades79@gmail.com>
2017-09-08 09:42:25 -0400

    minor refactoring

--- ToyotaRFID/app/src/main/java/com/example/nzar/toyotarfid/DatabaseConnector.java
+++ ToyotaRFID/app/src/main/java/com/example/nzar/toyotarfid/DatabaseConnector.java
@@ -4,15 +4,23 @@
 package com.example.nzar.toyotarfid;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.AsyncTask;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
 import android.util.JsonReader;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
@@ -36,7 +44,7 @@ and is also responsible for sending the appropriate data back to the database fo
 */
 class DatabaseConnector extends AppCompatActivity {
 
-    public static ArrayList<String> PPEList = new ArrayList<>();
+    public static ArrayList<PPE> PPEList = new ArrayList<>();
     public static ArrayList<LabTech> LabTechList = new ArrayList<>();
     public static int currentSessionID;
     public static String baseServerUrl = "10.2.5.50";
@@ -48,9 +56,19 @@ class DatabaseConnector extends AppCompatActivity {
         String lastName;
         String email;
         String phoneNumber;
+        Drawable Image;
       }
 
-      
+    static class PPE {
+        int PPEID;
+        String name;
+        Drawable Image;
+        boolean Required;
+        boolean Restricted;
+    }
+
+
+    @Nullable
     private static JsonReader TILTAPITask(HttpURLConnection connection, String method) throws Exception {
         connection.setRequestMethod(method);
         connection.setRequestProperty("Authorization", "basic VElMVFdlYkFQSToxM1RJTFRXZWJBUEkxMw==");
@@ -70,6 +88,11 @@ class DatabaseConnector extends AppCompatActivity {
         }
     }
 
+    private static Drawable ImageParser(String jsonImage) throws UnsupportedEncodingException {
+        byte encodedImage[] = jsonImage.getBytes();
+        Bitmap bitmap = BitmapFactory.decodeByteArray(encodedImage, 15, encodedImage.length);
+        return new BitmapDrawable(Resources.getSystem(), bitmap);
+    }
 
 //give the badge number as a string, provide progress messages as Strings, and return a Boolean if the user is allowed
     static class TILTPostUserTask extends AsyncTask<String, String, Boolean> {
@@ -93,7 +116,7 @@ class DatabaseConnector extends AppCompatActivity {
             try {
                 URL url = new URL("http://" +
                         baseServerUrl +
-                        "/tiltwebapi/api/Users?" +
+                        "/tiltwebapi/api/users?" +
                         "sessionID=" + sessionID +
                         "&machineIP=" + machineIP +
                         "&badgeID=" + badgeID +
@@ -110,15 +133,31 @@ class DatabaseConnector extends AppCompatActivity {
 
                 PPEList.clear();
                 while(Response.hasNext()) {
+                    PPE ppe = new PPE();
                     Response.beginObject();
                     while (Response.hasNext()) {
                         //parse response for PPE info
                         //if the response is not empty, set UserAuthorized to true
                         String key = Response.nextName();
-                        if (key.equals("PPE")) {
-                            PPEList.add(Response.nextString());
-                        } else {
-                            Response.skipValue();
+                        switch (key) {
+                            case "PPEID":
+                                ppe.PPEID = Response.nextInt();
+                                break;
+                            case "PPE":
+                                ppe.name = Response.nextString();
+                                break;
+                            case "Image":
+                                ppe.Image = ImageParser(Response.nextString());
+                                break;
+                            case "Required":
+                                ppe.Required = Response.nextBoolean();
+                                break;
+                            case "Restricted":
+                                ppe.Restricted = Response.nextBoolean();
+                                break;
+                            default:
+                                Response.skipValue();
+                                break;
                         }
                     }
                     Response.endObject();
@@ -207,8 +246,10 @@ class DatabaseConnector extends AppCompatActivity {
                                 temp.email = ResponseReader.nextString();
                                 break;
                             case ("PhoneNumber"):
-                                temp.email = ResponseReader.nextString();
+                                temp.phoneNumber = ResponseReader.nextString();
                                 break;
+                            case "Image":
+                                temp.Image = ImageParser(ResponseReader.nextString());
                             default:
                                 ResponseReader.skipValue();
                                 break;
@@ -226,105 +267,6 @@ class DatabaseConnector extends AppCompatActivity {
 
             return null;
         }
-    }
-
-    //take the app context, doesn't provide updates, and return a boolean
-    public static class SetNetworkConfigTask extends AsyncTask<Context,Void,Boolean> {
-        @Override
-        protected Boolean doInBackground(Context... params) {
-
-            WifiConfiguration wifiConf = null;
-            WifiManager wifiManager = (WifiManager) params[0].getSystemService(Context.WIFI_SERVICE);
-            WifiInfo connectionInfo = wifiManager.getConnectionInfo();
-            List<WifiConfiguration> configuredNetworks = wifiManager.getConfiguredNetworks();
-            for (WifiConfiguration conf : configuredNetworks) {
-                if (conf.networkId == connectionInfo.getNetworkId()) {
-                    wifiConf = conf;
-                    break;
-                }
-            }
-            try {
-                NetworkConfigurator.setIpAssignment("STATIC", wifiConf); //or "DHCP" for dynamic setting
-                NetworkConfigurator.setIpAddress(InetAddress.getByName(settings.getString("static_ip", "192.168.0.235")), 24, wifiConf);
-                NetworkConfigurator.setDNS(InetAddress.getByName("8.8.8.8"), wifiConf);
-                wifiManager.updateNetwork(wifiConf); //apply the setting
-                wifiManager.saveConfiguration(); //Save it
-            } catch (Exception e) {
-                e.printStackTrace();
-                return false;
-            }
-
-            return true;
-        }
-    }
-
-
-    @SuppressWarnings("unchecked")
-    private static class NetworkConfigurator {
-
-        static void setIpAssignment(String assign, WifiConfiguration wifiConf)
-                throws SecurityException, IllegalArgumentException, NoSuchFieldException, IllegalAccessException {
-            setEnumField(wifiConf, assign, "ipAssignment");
-        }
-
-        static void setIpAddress(InetAddress addr, int prefixLength, WifiConfiguration wifiConf)
-                throws SecurityException, IllegalArgumentException, NoSuchFieldException, IllegalAccessException,
-                NoSuchMethodException, ClassNotFoundException, InstantiationException, InvocationTargetException {
-            Object linkProperties = getField(wifiConf, "linkProperties");
-            if (linkProperties == null) return;
-            Class laClass = Class.forName("android.net.LinkAddress");
-            Constructor laConstructor = laClass.getConstructor(InetAddress.class, int.class);
-            Object linkAddress = laConstructor.newInstance(addr, prefixLength);
-
-            ArrayList mLinkAddresses = (ArrayList) getDeclaredField(linkProperties, "mLinkAddresses");
-            mLinkAddresses.clear();
-            mLinkAddresses.add(linkAddress);
-        }
-
-        public static void setGateway(InetAddress gateway, WifiConfiguration wifiConf)
-                throws SecurityException, IllegalArgumentException, NoSuchFieldException, IllegalAccessException,
-                ClassNotFoundException, NoSuchMethodException, InstantiationException, InvocationTargetException {
-            Object linkProperties = getField(wifiConf, "linkProperties");
-            if (linkProperties == null) return;
-            Class routeInfoClass = Class.forName("android.net.RouteInfo");
-            Constructor routeInfoConstructor = routeInfoClass.getConstructor(InetAddress.class);
-            Object routeInfo = routeInfoConstructor.newInstance(gateway);
-
-            ArrayList mRoutes = (ArrayList) getDeclaredField(linkProperties, "mRoutes");
-            mRoutes.clear();
-            mRoutes.add(routeInfo);
-        }
-
-        static void setDNS(InetAddress dns, WifiConfiguration wifiConf)
-                throws SecurityException, IllegalArgumentException, NoSuchFieldException, IllegalAccessException {
-            Object linkProperties = getField(wifiConf, "linkProperties");
-            if (linkProperties == null) return;
-
-            ArrayList<InetAddress> mDnses = (ArrayList<InetAddress>) getDeclaredField(linkProperties, "mDnses");
-            mDnses.clear(); //or add a new dns address , here I just want to replace DNS1
-            mDnses.add(dns);
-        }
-
-        static Object getField(Object obj, String name)
-                throws SecurityException, NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
-            Field f = obj.getClass().getField(name);
-            return f.get(obj);
-        }
-
-        static Object getDeclaredField(Object obj, String name)
-                throws SecurityException, NoSuchFieldException,
-                IllegalArgumentException, IllegalAccessException {
-            Field f = obj.getClass().getDeclaredField(name);
-            f.setAccessible(true);
-            return f.get(obj);
-        }
-
-        static void setEnumField(Object obj, String value, String name)
-                throws SecurityException, NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
-            Field f = obj.getClass().getField(name);
-            f.set(obj, Enum.valueOf((Class<Enum>) f.getType(), value));
-        }
-
     }
 
 
