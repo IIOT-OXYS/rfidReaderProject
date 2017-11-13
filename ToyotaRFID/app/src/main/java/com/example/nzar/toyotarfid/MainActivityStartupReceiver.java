4a68d93443983ea98c85017f2cd7f0c2d3e2cbb2
arades79 <arades79@gmail.com>
2017-11-13 18:30:53 -0500

    add comments

--- ToyotaRFID/app/src/main/java/com/example/nzar/toyotarfid/MainActivityStartupReceiver.java
+++ ToyotaRFID/app/src/main/java/com/example/nzar/toyotarfid/MainActivityStartupReceiver.java
@@ -4,16 +4,18 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+import java.util.Objects;
+
 /**
  * Created by uberm on 9/16/2017.
  */
 
 public class MainActivityStartupReceiver extends BroadcastReceiver {
 
-
+    //  start application on device startup
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
+        if (Objects.equals(intent.getAction(), Intent.ACTION_BOOT_COMPLETED)) {
             Intent startupIntent = new Intent(context, MainActivity.class);
             startupIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             context.startActivity(startupIntent);
